298eb0f90e994b4600b2ef00d58c11c146eb8f19
Bulbak1 <shmygovskii@mail.ru>
2025-03-04 21:15:11 +0300

    Add files via upload

---
+++ Stack.h
@@ -0,0 +1,28 @@
+#include <iostream>
+
+class Node {
+private:
+	char value;
+	Node* next = nullptr;
+public:
+	Node() = default;
+	Node(char);
+	~Node() = default;
+	friend class Stack;
+};
+
+class Stack {
+private:
+	Node* root = nullptr;
+	int size = 0;
+public:
+	Stack() = default;
+	Stack(char);
+	~Stack();
+	void push(char);
+	char pop();
+	char top() const;
+
+	bool is_empty() const;
+	void clear();
+};
